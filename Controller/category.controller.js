349b772e6bbbd4f2d1d3ed981f1a6e60ddfb2297
omkar <omkarsonawaneomkar2@gmail.com>
2023-02-04 20:40:50 +0530

    config and category method

--- Controller/category.controller.js
+++ Controller/category.controller.js
@@ -1,69 +1,30 @@
-const db = require('./../model/index')
+const db = require('./../model/index');
 
-
-const addCategory = async(req,res)=>{
-    let categories = await db.category.create({
-        category:req.body.name
-    })
-    res.status(200).json({msg:"Category added Successfully"})
-    res.end()
-}
-
+const addCategory = async (req, res) => {
+  let categories = await db.category.create({
+    category: req.body.name,
+  });
+  res.status(200).json({ msg: 'Category added Successfully' });
+  res.end();
+};
 
 //this is for get all categories
-const getAllCategory = async(req,res)=>{
-    res.status(200).json({msg:"all Categories"})
-    res.end()
-}
+const getAllCategory = async (req, res) => {
+  res.status(200).json({ msg: 'all Categories' });
+  res.end();
+};
 
 //this is for suggestion
-const getCategoryById = async(req,res)=>{
-    try{
-
-        let categoryId = req.params.id
-        let category = await db.category.findOne({where:{
-            id : categoryId
-        }})
-        let getAllProduct = await category.getProducts({offset:Math.floor((Math.random()*20)),limit:15})
-        res.status(200).json(getAllProduct);
-        res.end()
-    } catch(err){
-        res.status(400).json(err)
-        res.end()
-        
-    }
-}
-
-//this is for home page demo products
-const getCategoryByManyIds = async(req,res)=>{
-    try{
-
-        let categoryIds = req.body.ids
-        let allProducts = []
-        for (let i = 0; i < categoryIds.length; i++) {
-            let category = await db.category.findOne({where:{
-                id : categoryIds[i]
-            }})
-            let getAllProduct = await category.getProducts({offset:Math.floor((Math.random()*10)),limit:15})
-            allProducts.push(getAllProduct)
-        }
-        res.status(200).json(allProducts);
-        res.end()
-    } catch(err){
-        res.status(400).json(err)
-        res.end()
-
-    }
-}
-
-
-
-const searchByCategory = async (req, res) => {
+const getCategoryById = async (req, res) => {
   try {
     let categoryId = req.params.id;
-    let category = await db.category.findByPk(categoryId);
-    let getAllProduct = await category.getProducts();
-    res.status(200).json({products:getAllProduct,category:category});
+    let category = await db.category.findOne({
+      where: {
+        id: categoryId,
+      },
+    });
+    let getAllProduct = await category.getProducts({ offset: Math.floor(Math.random() * 20), limit: 15 });
+    res.status(200).json(getAllProduct);
     res.end();
   } catch (err) {
     res.status(400).json(err);
@@ -71,6 +32,44 @@ const searchByCategory = async (req, res) => {
   }
 };
 
+//this is for home page demo products
+const getCategoryByManyIds = async (req, res) => {
+  try {
+    let categoryIds = req.body.ids;
+    let allProducts = [];
+    for (let i = 0; i < categoryIds.length; i++) {
+      let category = await db.category.findOne({
+        where: {
+          id: categoryIds[i],
+        },
+      });
+      let getAllProduct = await category.getProducts({ offset: Math.floor(Math.random() * 10), limit: 15 });
+      allProducts.push(getAllProduct);
+    }
+    res.status(200).json(allProducts);
+    res.end();
+  } catch (err) {
+    res.status(400).json(err);
+    res.end();
+  }
+};
 
-module.exports = {addCategory, getAllCategory,searchByCategory , getCategoryById, getCategoryByManyIds}
+const searchByCategory = async (req, res) => {
+  try {
+    let categoryId = req.params.id;
+    if (categoryId == 'all') {
+      let getAllProduct = await db.product.findAll();
+      res.status(200).json({ products: getAllProduct, category: 'All' });
+      return;
+    }
+    let category = await db.category.findByPk(categoryId);
+    let getAllProduct = await category.getProducts();
+    res.status(200).json({ products: getAllProduct, category: category });
+    res.end();
+  } catch (err) {
+    res.status(400).json(err);
+    res.end();
+  }
+};
 
+module.exports = { addCategory, getAllCategory, searchByCategory, getCategoryById, getCategoryByManyIds };
--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,18 @@
 const developmentInstance = {
+  DB: 'rajdhani',
+  USER: 'root',
+  PASSWORD: 'omkar',
+  DIALECT: 'mysql',
+  HOST: 'localhost',
+  // PORT: 7825,
+  POOL: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+const productionInstance = {
   DB: 'railway',
   USER: 'root',
   PASSWORD: 'Z0w4XVsTHU2Xr4zwnifb',
@@ -15,4 +29,5 @@ const developmentInstance = {
 
 module.exports = {
   development: developmentInstance,
+  production: productionInstance,
 };
